288c6bfe3a920534f97bb6580917dde252202665
yonghwan98 <bbl737898@gmail.com>
2022-03-21 10:48:50 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const home = require("./routes/home");
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home);
+
+module.exports = app;
